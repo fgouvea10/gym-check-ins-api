2624879ceb486a601e065d2ba75b9c702cbf590c
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-17 23:44:07 -0300

    feat: create authentication service

---
+++ src/http/services/authenticate.service.ts
@@ -0,0 +1,35 @@
+import { compare } from 'bcrypt'
+import type { User } from '@prisma/client'
+
+import { UsersRepository } from '~/repositories/interfaces/users-repository'
+import { InvalidCredentialsException } from './errors/invalid-credentials-error'
+
+interface AuthenticateServiceRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateServiceResponse {
+  user: User
+}
+
+export class AuthenticateService {
+  constructor(private repository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+    const user = await this.repository.findByEmail(email)
+
+    if (!user) throw new InvalidCredentialsException()
+
+    const doesPasswordMatches = await compare(password, user.passwordHash)
+
+    if (!doesPasswordMatches) throw new InvalidCredentialsException()
+
+    return {
+      user
+    }
+  }
+}
