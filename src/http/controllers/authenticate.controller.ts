4869747af6e2e8f9c647e6ce55893481af521e5e
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-25 11:43:49 -0300

    feat: implement jwt auth

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -15,7 +15,17 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   try {
     const authenticateService = makeAuthenticateService()
 
-    await authenticateService.execute({ email, password })
+    const { user } = await authenticateService.execute({ email, password })
+
+    const token = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id,
+      }
+    })
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsException) return reply.status(400).send({
       message: err.message
@@ -23,6 +33,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
     throw err
   }
-
-  return reply.status(200).send()
 }
